ad4bd711d276e6385bb15f3cbdf8d076598e52aa
ricardo-ljr <ricardo.leitejr95@gmail.com>
2020-01-22 16:15:53 -0700

    Finished MVP

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=t3F5j6ddCIh7btoXDgbTFmLp5COSAOfvqvQ03bUd&date=&date=2012-03-14`
+      )
+      .then(response => {
+        console.log(response);
+      });
+  }, []);
   return (
     <div className="App">
       <p>
